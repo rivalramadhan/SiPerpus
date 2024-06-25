4adf370df1e6b5fb0572b22e9c3ab946faefbd40
rivalramadhan <ripallez@gmail.com>
2024-06-25 14:17:04 +0700

    feat(rentbook): function add data rentbook

--- app/Http/Controllers/RentBookController.php
+++ app/Http/Controllers/RentBookController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\RentBookResource;
 use App\Models\RentBook;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RentBookController extends Controller
 {
@@ -14,4 +15,25 @@ class RentBookController extends Controller
         return (new RentBookResource(true, "list data", $rentbook))->response()->setStatusCode(200);
     }
     
+    public function create(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'book_title' => ['required', 'String'],
+            'fullname' => ['required', 'String'],
+            'rent_date' => ['required', 'date'],
+            'return_date' => ['required', 'date'],
+            'status' => ['required', 'string', 'max:100']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $rentbook = RentBook::create([
+            'book_title' => $request->book_title,
+            'fullname' => $request->fullname,
+            'rent_date' => $request->rent_date,
+            'return_date' => $request->return_date,
+            'status' => $request->status
+        ]);
+        return (new RentBookResource(true, "data created", $rentbook));
+    }
 }
