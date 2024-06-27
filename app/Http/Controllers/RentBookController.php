1d31668bc3cfa4ab7d6f19c2b3c215aa2b089c9b
rivalramadhan <ripallez@gmail.com>
2024-06-27 19:01:36 +0700

    feat: update method for rentbook

--- app/Http/Controllers/RentBookController.php
+++ app/Http/Controllers/RentBookController.php
@@ -36,5 +36,38 @@ class RentBookController extends Controller
         ]);
         return (new RentBookResource(true, "data created", $rentbook));
     }
-    public function update(Request $request) {}
+    public function delete($id)
+    {
+        $book = RentBook::find($id);
+        $book->delete();
+        return new RentBookResource(true, 'Data Rent Book Berhasil Dihapus!', null);
+    }
+    public function show($id)
+    {
+        $book = RentBook::find($id);
+        return new RentBookResource(true, 'Detail Data Rent Book', $book);
+    }
+    public function update(Request $request, $id) {
+        $rentbook = RentBook::find($id);
+        $validator = Validator::make($request->all(), [
+            'book_title' => ['required', 'String'],
+            'fullname' => ['required', 'String'],
+            'rent_date' => ['required', 'date'],
+            'return_date' => ['required', 'date'],
+            'status' => ['String']
+        ]);
+        
+        if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
+        }
+            $rentbook->update([
+                'book_title' => $request->book_title,
+                'fullname' => $request->fullname,
+                'rent_date' => $request->rent_date,
+                'return_date' => $request->return_date,
+                'status' => $request->status
+            ]);
+            return new RentBookResource(true,'Update berhasil', $rentbook);
+        }
+    
 }
